4e80e3cbed0bd97012c03a4cecc020fc9aa21d9b
dhondta <alexandre.dhondt@gmail.com>
2017-07-13 15:39:52 +0200

    Fine-tuned setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,16 @@ setup(
   url = "https://github.com/dhondta/tinyscript",
   download_url = "https://github.com/dhondta/tinyscript/archive/0.1.tar.gz",
   keywords = ["CLI tool", "logging"],
-  classifiers = [],
-  zip_safe=False,
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+  ],
+  python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
 )
