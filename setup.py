69d4896f4a32fc901832bd075f6e88b6281757ce
dhondta <alexandre.dhondt@gmail.com>
2018-09-14 10:13:50 +0200

    Minor addition to helpers

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from setuptools import setup
 setup(
   name = "tinyscript",
   packages = ["tinyscript"],
-  version = "0.3.16",
+  version = "0.3.17",
   license = "GPLv3",
   description = "A tiny library for easily building \"self-contained\" CLI "
                 "Python tools with base features in a shortened way",
   author = "Alexandre D\'Hondt",
   author_email = "alexandre.dhondt@gmail.com",
   url = "https://github.com/dhondta/tinyscript",
-  download_url = "https://github.com/dhondta/tinyscript/archive/0.3.16.tar.gz",
+  download_url = "https://github.com/dhondta/tinyscript/archive/0.3.17.tar.gz",
   keywords = ["CLI tool", "logging"],
   classifiers = [
     'Development Status :: 3 - Alpha',
--- tinyscript/helpers.py
+++ tinyscript/helpers.py
@@ -22,5 +22,7 @@ int2bin = lambda i, n=None: ("{}" if n is None else "{:0>" + str(n) + "}") \
 txt2bin = lambda t: ''.join(map(lambda c: "{:0>8}".format(bin(ord(c))[2:]), t))
 
 is_str = lambda s: isinstance(s, string_types)
-is_bin = lambda b: all(str(_) in "01" for _ in b)
 is_lst = lambda l: isinstance(l, (list, tuple))
+
+is_bin = lambda b: (is_str or is_lst) and all(str(_) in "01" for _ in b)
+is_hex = lambda h: is_str and all(_ in "0123456789abcdef" for _ in h.lower())
