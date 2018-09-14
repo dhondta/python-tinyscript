f3a4d35edcb64ab4b3dddd0dc4980a76ee002bab
dhondta <alexandre.dhondt@gmail.com>
2018-09-14 09:55:05 +0200

    Added helper lambda functions

---
+++ tinyscript/helpers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Common help utility functions.
+
+"""
+
+__author__ = "Alexandre D'Hondt"
+__version__ = "1.0"
+__copyright__ = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
+__all__ = ["bin2int", "bin2txt", "int2bin", "txt2bin",
+           "is_bin", "is_str", "is_lst"]
+
+
+bin2int = lambda b: int("0b" + b, 2)
+bin2txt = lambda b: ''.join(chr(int2bin(''.join(map(str, b[i:i+8]))) \
+                    for i in range(0, len(b), 8))
+int2bin = lambda i, n=None: ("{}" if n is None else "{:0>" + str(n) + "}") \
+                            .format(bin(i)[2:])
+txt2bin = lambda t: ''.join(map(lambda c: "{:0>8}".format(bin(ord(c))[2:]), t))
+
+is_str = lambda s: isinstance(s, string_types)
+is_bin = lambda b: is_str(b) and all(_ in "01" for _ in b)
+is_lst = lambda b: isinstance(s, (list, tuple))
