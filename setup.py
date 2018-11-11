ff59bfc7cee1d7a76ff5ac0ed0c559705eaab399
dhondta <alexandre.dhondt@gmail.com>
2018-11-11 20:31:13 +0100

    Fixed minor bug

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ setup(
   packages = find_packages(),
   author = "Alexandre D\'Hondt",
   author_email = "alexandre.dhondt@gmail.com",
-  version = "1.1.1",
+  version = "1.1.2",
   license = "AGPLv3",
   url = "https://github.com/dhondta/tinyscript",
-  download_url = "https://github.com/dhondta/tinyscript/archive/1.1.1.tar.gz",
+  download_url = "https://github.com/dhondta/tinyscript/archive/1.1.2.tar.gz",
   description = "A library for quickly building CLI Python-based tools with "
                 "basic features in a shortened way",
   long_description=long_descr,
