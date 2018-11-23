45f8b9804b7646bac2b82fb47398703bbf6467f6
dhondta <alexandre.dhondt@gmail.com>
2018-11-23 16:35:55 +0100

    Refined helpers.utils

--- tinyscript/helpers/utils.py
+++ tinyscript/helpers/utils.py
@@ -4,15 +4,33 @@
 
 """
 import re
+from humanfriendly.terminal import ansi_wrap
 
 from .lambdas import is_lambda
 from ..__info__ import __author__, __copyright__, __version__
 
 
-__all__ = __features__ = ["user_input"]
+__all__ = __features__ = ["std_input", "user_input"]
 
 
 REGEX = re.compile(r'^\(([a-z0-9])\).*$', re.I)
+
+
+def std_input(prompt="", style=None):
+    """
+    Very simple Python2/3-compatible input method.
+    
+    :param prompt:  prompt message
+    :param style:   dictionary of ansi_wrap keyword-arguments
+    """
+    try:
+        prompt = ansi_wrap(prompt, **style)
+    except:
+        pass
+    try:
+        return raw_input(prompt).strip()
+    except NameError:
+        return input(prompt).strip()
 
 
 def user_input(prompt="", choices=None, default=None, choices_str=""):
@@ -36,10 +54,7 @@ def user_input(prompt="", choices=None, default=None, choices_str=""):
         _check = lambda v: True
     default_str = " [{}]".format(default) if default else ""
     prompt += "{}{}\n >> ".format(choices_str, default_str)
-    try:
-        user_input = raw_input(prompt).strip()
-    except NameError:
-        user_input = input(prompt).strip()
+    user_input = std_input(prompt)
     if type(choices) in [list, tuple, set]:
         user_input = user_input.lower()
     if user_input == "" and default is not None and _check(default):
