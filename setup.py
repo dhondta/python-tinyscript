929d7d8cecb30295c2f62d2f8bb522bc83619ff1
dhondta <alexandre.dhondt@gmail.com>
2018-11-24 12:12:35 +0100

    New release

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ setup(
   packages = find_packages(),
   author = "Alexandre D\'Hondt",
   author_email = "alexandre.dhondt@gmail.com",
-  version = "1.5.1",
+  version = "1.5.2",
   license = "AGPLv3",
   url = "https://github.com/dhondta/tinyscript",
-  download_url = "https://github.com/dhondta/tinyscript/archive/1.5.1.tar.gz",
+  download_url = "https://github.com/dhondta/tinyscript/archive/1.5.2.tar.gz",
   description = "A library for quickly building CLI Python-based tools with "
                 "basic features in a shortened way",
   long_description=long_descr,
