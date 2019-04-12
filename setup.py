c87f87a7aa7934ba7e76baf9dc14e5498b167d6b
dhondta <alexandre.dhondt@gmail.com>
2019-04-12 16:33:46 +0200

    Updated Python versions in setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
@@ -55,5 +54,5 @@ setup(
     "six",
     "weasyprint ; python_version>='3.4'",
   ],
-  python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,<4',
+  python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*!=3.4.*,<4',
 )
