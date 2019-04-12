53eee042a2a6c9cbead6e0e3fe16f78100461c3c
dhondta <alexandre.dhondt@gmail.com>
2019-04-12 12:04:52 +0200

    New releade

--- tinyscript/__info__.py
+++ tinyscript/__info__.py
@@ -5,7 +5,7 @@
 """
 
 __author__    = "Alexandre D'Hondt"
-__version__   = "1.8.1"
+__version__   = "1.9.0"
 __copyright__ = "© 2019 A. D'Hondt"
 __license__   = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
 
