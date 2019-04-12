24aa135180cef29a051309a4bb8d453b60c1c9fb
dhondta <alexandre.dhondt@gmail.com>
2019-04-12 08:55:52 +0200

    Refined __info__

--- tinyscript/__info__.py
+++ tinyscript/__info__.py
@@ -4,7 +4,8 @@
 
 """
 
-__author__ = "Alexandre D'Hondt"
-__version__ = "1.8.1"
-__copyright__ = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
+__author__    = "Alexandre D'Hondt"
+__version__   = "1.8.1"
+__copyright__ = "© 2019 A. D'Hondt"
+__license__   = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
 
