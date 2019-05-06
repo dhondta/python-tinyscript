eddc9efa745f50301ba59cde92b9627d0bee6664
dhondta <alexandre.dhondt@gmail.com>
2019-05-06 23:03:28 +0200

    Improved code helpers

--- tests/test_helpers_code.py
+++ tests/test_helpers_code.py
@@ -6,21 +6,65 @@
 from unittest import TestCase
 
 from tinyscript.helpers.code import *
+from tinyscript.helpers.code import ReplacementFormatError
 
 from utils import *
 
 
+def dummy1(): return 1
+
+
+def dummy2():
+    # useless comment
+    return 1
+
+
+def dummy3():
+    # version: 1
+    return 1
+
+
 class TestHelpersCode(TestCase):
     def test_function_replacement(self):
-        temp_stdout(self)
-        def dummy(): return 1
-        self.assertEqual(dummy(), 1)
-        code_replace(dummy, "def dummy(): return 1", "def dummy(): return 42")
-        self.assertEqual(dummy(), 42)
-        def dummy(): return 1
-        code_replace(dummy, "return 1", "return 42")
-        self.assertEqual(dummy(), 42)
-        code_restore(dummy)
-        self.assertEqual(dummy(), 1)
-        def dummy(): return 1
-        self.assertRaises(ValueError, code_replace, dummy, "bad")
+        # this will use patchy.replace
+        code_replace(dummy1, "def dummy1(): return 1", "def dummy1(): return 42")
+        self.assertEqual(dummy1(), 42)
+        # this will trigger string replacements before using patchy.replace
+        code_replace(dummy1, "return 42", "return 2*42")
+        self.assertEqual(dummy1(), 84)
+        code_replace(dummy1, "return 2*42", "return 3*42")
+        self.assertEqual(dummy1(), 126)
+        # this will trigger an error as the replacement has a bad format
+        self.assertRaises(ReplacementFormatError, code_replace, dummy1, "bad")    
+
+    def test_function_line_replacement(self):
+        # this will modify lines by index
+        code_replace_lines(dummy2, 1, None, 2, "return 42")
+        self.assertEqual(dummy2(), 42)
+        # this will trigger an error as the replacement has a bad format
+        self.assertRaises(ReplacementFormatError, code_replace_lines, dummy2, 0)
+    
+    def test_function_code_restore(self):
+        # no modification yet ; code_restore should return False
+        self.assertFalse(code_restore(dummy3))
+        # now apply some modifications
+        self.assertIn("version: 1", code_source(dummy3))
+        code_replace_lines(dummy3, 1, "# version: 2")
+        self.assertIn("version: 2", code_source(dummy3))
+        code_replace_lines(dummy3, 1, "# version: 3")
+        self.assertIn("version: 3", code_source(dummy3))
+        code_replace_lines(dummy3, 1, "# version: 4")
+        self.assertIn("version: 4", code_source(dummy3))
+        code_replace_lines(dummy3, 1, "# version: 5")
+        self.assertIn("version: 5", code_source(dummy3))
+        # test maximum 3 reverts
+        self.assertTrue(code_revert(dummy3))
+        self.assertTrue(code_revert(dummy3))
+        self.assertTrue(code_revert(dummy3))
+        self.assertFalse(code_revert(dummy3))
+        # now replace once again and try to restore
+        code_replace_lines(dummy3, 1, "# version: 7")
+        self.assertIn("version: 7", code_source(dummy3))
+        code_restore(dummy3)
+        self.assertIn("version: 1", code_source(dummy3))
+        self.assertEqual(dummy3(), 1)
--- tinyscript/helpers/code.py
+++ tinyscript/helpers/code.py
@@ -3,50 +3,143 @@
 """Common code-related functions.
 
 """
-import inspect
 import patchy
+from collections import deque
+from six import string_types
 
 
 __all__ = __features__ = ["code_patch", "code_unpatch", "CodePatch",
-                          "code_replace", "code_restore"]
+                          "code_replace", "code_replace_lines",
+                          "code_restore", "code_revert", "code_source"]
+
+
+N_MODIF = 3
 
 
 code_patch   = patchy.patch
 code_unpatch = patchy.unpatch
-CodePatch    = patchy.temp_patch
+CodePatch    = patchy.temp_patch 
 
 
-__modified_code = {}
+__orig_code = {}
+__old_code = {}
 
 
+class ReplacementFormatError(ValueError):
+    pass
+
+
+def cache(f):
+    def _wrapper(*args, **kwargs):
+        cache = kwargs.pop("cache", True)
+        if cache:
+            func = args[0]
+            old_code = code_source(func)
+        f(*args)
+        if cache:
+            if __orig_code.get(func) is None:
+                __orig_code[func] = old_code
+            if __old_code.get(func) is None:
+                __old_code[func] = deque([], N_MODIF)
+            __old_code[func].append(old_code)
+    return _wrapper
+
+
+@cache
 def code_replace(func, *replacements):
-    """ Slight modification to original replace command to allow replacing only
-         part(s) of code and not necessarily using whole function's code. """
+    """
+    Slight modification to original replace function to allow replacing only
+     part(s) of code and not necessarily using whole function's code.
+    
+    :param func:         function object to be modified
+    :param replacements: list of replacements - pairs (str_to_repl, replac)
+    """
     try:
         patchy.replace(func, *replacements)
-        old_code = replacements[0]
     # if normal handling fails, get whole function's code first, apply
     #  replacement then retry
     except:
         if len(replacements) % 2 != 0:
-            raise ValueError("Bad code replacements")
-        old_code = inspect.getsource(func)
+            raise ReplacementFormatError("Bad code replacements")
+        old_code = code_source(func)
+        new_code = old_code
         for i in range(0, len(replacements), 2):
-            new_code = old_code.replace(replacements[i], replacements[i+1])
+            new_code = new_code.replace(replacements[i], replacements[i+1])
         patchy.replace(func, old_code, new_code)
-    __modified_code[func] = old_code
+
+
+@cache
+def code_replace_lines(func, *replacements):
+    """
+    Additional replace function to allow replacing only specific lines in the
+     code of a function.
+    
+    :param func: function object to be modified
+    :param replacements: list of replacements - pairs (str_to_repl, replac)
+    """
+    r = replacements
+    if len(r) % 2 != 0 or not \
+        all(isinstance(r[i], int) and (isinstance(r[i+1], string_types) or \
+            r[i+1] is None) for i in range(0, len(r), 2)):
+        raise ReplacementFormatError("Bad code replacements")
+    old_code = code_source(func)
+    new_code = old_code.splitlines()
+    delete = []
+    for i in range(0, len(r), 2):
+        n, replacement = r[i:i+2]
+        if replacement is None:
+            delete.append(n)
+        else:
+            indent = new_code[n][:len(new_code[n])-len(new_code[n].lstrip())]
+            new_code[n] = indent + replacement.lstrip()
+    for i in sorted(delete, reverse=True):
+        del new_code[i]
+    patchy.api._set_source(func, "\n".join(new_code))
 
 
 def code_restore(func):
-    """ Additional function to restore mofidied code. """
-    old_code = __modified_code.get(func)
+    """
+    Additional function to restore mofidied code.
+    
+    :param func: function object to be restored
+    :return:     True if function's code was restored
+    """
+    old_code = __orig_code.get(func)
     if old_code is not None:
-        old_code = patchy.api.dedent(old_code)
-        patchy.api._set_source(func, old_code)
+        patchy.api._set_source(func, patchy.api.dedent(old_code))
+        __old_code[func] = deque([], N_MODIF)
+        return True
+    return False
 
 
-# disable source AST check in _assert_ast_equal to avoid Python compatibility
-#  errors
+def code_revert(func):
+    """ 
+    Additional function to revert mofidied code to last modification.
+    
+    :param func: function object to be reverted to last version
+    :return:     True if function's code was reverted to last version
+    """
+    try:
+        old_code = __old_code.get(func).pop()
+        if old_code is not None:
+            patchy.api._set_source(func, patchy.api.dedent(old_code))
+            return True
+    except:
+        return False
+
+
+def code_source(func):
+    """ 
+    Alias function to get function's code.
+    
+    :param func: function object
+    :return:     function's source code
+    """
+    return patchy.api._get_source(func)
+
+
+# disable source AST check to avoid Python compatibility errors
 code_replace(patchy.api._assert_ast_equal,
              "current_ast = ast.parse",
-             "return #current_ast = ast.parse")
+             "return #current_ast = ast.parse",
+             cache=False)
