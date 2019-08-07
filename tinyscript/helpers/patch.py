6c83e71fd0bc60a1c88513a3eed1717296fd42bf
dhondta <alexandre.dhondt@gmail.com>
2019-08-07 17:38:54 +0200

    Improved helpers.patch

--- tinyscript/helpers/patch.py
+++ tinyscript/helpers/patch.py
@@ -8,14 +8,14 @@ from collections import deque
 from six import string_types
 
 
-__all__ = __features__ = ["code_patch", "code_unpatch", "CodePatch",
-                          "code_add_line", "code_add_lines",
-                          "code_delete_line", "code_delete_lines",
-                          "code_insert_line", "code_insert_lines",
-                          "code_remove_line", "code_remove_lines",
-                          "code_replace", "code_replace_line",
-                          "code_replace_lines", "code_restore", "code_revert",
-                          "code_source"]
+__all__ = __features__ = [
+    "code_patch", "code_unpatch", "CodePatch", "code_add_block",
+    "code_add_line", "code_add_lines", "code_delete_line", "code_delete_lines",
+    "code_insert_block", "code_insert_line", "code_insert_lines",
+    "code_remove_line", "code_remove_lines", "code_replace",
+    "code_replace_line", "code_replace_lines", "code_restore", "code_revert",
+    "code_source",
+]
 
 
 BLOCK_KW = ["class", "def", "elif", "else", "except", "finally", "for", "if",
@@ -36,7 +36,18 @@ class PatchError(ValueError):
     pass
 
 
+def __apply_code(func, old_code, new_code):
+    """ Apply the new code in the given function object. """
+    new_code = "\n".join(new_code)
+    try:
+        patchy.api._set_source(func, new_code)
+    except (IndentationError, SyntaxError) as e:
+        raise PatchError(e)
+    return old_code != new_code
+
+
 def __get_indent_chars(code_lines):
+    """ Guess the indentation characters used in the given code. """
     prev, new = 0, 0
     n, l2 = len(code_lines) - 1, ""
     while prev <= new and n >= 0:
@@ -50,6 +61,7 @@ def __get_indent_chars(code_lines):
     
 
 def __get_block_indent(code_lines, n):
+    """ Guess the indentation level used in the given code from line n. """
     n %= len(code_lines)
     code_lines[n]  # this will raise an error if n too high
     n -= 1
@@ -59,14 +71,46 @@ def __get_block_indent(code_lines, n):
         n -= 1
         l = code_lines[n].split("#", 1)[0].rstrip()  # remove comment
     indent = l[:len(l) - len(l.lstrip())]
-    # if a block keyword is present, return block's indentation
-    if any(l.lstrip().startswith(kw) for kw in BLOCK_KW):
-        return indent + __get_indent_chars(code_lines)
-    # else, return same indentation
-    return indent
+    # if a block keyword is present, return block's indentation ; otherwise,
+    #  return the same indentation
+    return indent + __get_indent_chars(code_lines) \
+        if any(l.lstrip().startswith(kw) for kw in BLOCK_KW) else indent
+
+
+def __sort_int_text_pairs(text, lst, item):
+    """ Check that l only consists of flattened (int, str or None) pairs and 
+         that integers are within the range of the input text. Then output the
+         list of sorted pairs. """
+    l = len(text.split("\n"))
+    s = "Bad code {}s".format(item)
+    if len(lst) % 2 != 0:
+        raise PatchError(s)
+    # check the input list based on multiple criteria
+    d = {}
+    for i in range(0, len(lst), 2):
+        n = lst[i]
+        # check for inconsistent index
+        if not isinstance(n, int):
+            raise PatchError(s + " (bad index {})".format(n))
+        n = lst[i] if lst[i] >= 0 else lst[i] + l
+        # check for out-of-bound values
+        if not 0 <= n < l:
+            raise PatchError(s + " (invalid index {})".format(n))
+        # check for doubles
+        if d.get(n) is not None:
+            raise PatchError(s + " (double index {})".format(n))
+        # check for inconsistent line
+        line = lst[i+1]
+        _ = [line, ""][line is None]
+        if line is not None and not isinstance(_, string_types):
+            raise PatchError(s + " (bad line '{}')".format(line))
+        d[n] = line
+    # then return the sorted list of pairs
+    return sorted(d.items(), reverse=True)
 
 
 def cache(f):
+    """ Decorator for caching code changes locally inside the module. """
     def _wrapper(*args, **kwargs):
         cache = kwargs.pop("cache", True)
         if cache:
@@ -80,6 +124,32 @@ def cache(f):
                 __old_code[func] = deque([], N_MODIF)
             __old_code[func].append(old_code)
     return _wrapper
+
+
+def code_add_block(func, index, block, after=False):
+    """
+    Additional modification function to allow adding a block of code at a
+     specific place in the code of a function. The input block is automatically
+     reindented relatively to its insertion position. Therefore, successive
+     distinct blocks of code with dedented levels SHALL NOT be added in the same
+     block value through this function but well separately.
+    
+    :param func:  function object to be modified
+    :param index: position where the block of code is to be added
+    :param block: block of code indented relatively to the insertion position
+    :param after: add line after the given index, not before
+    :return:      whether code was modified or not
+    """
+    # make the new code
+    old_code = code_source(func)
+    new_code = old_code.splitlines()
+    if after:
+        index += 1
+    indent = __get_block_indent(new_code, index)
+    for line in block.splitlines()[::-1]:
+        new_code.insert(index, indent + line)
+    return __apply_code(func, old_code, new_code)
+code_insert_block = code_add_block
 
 
 def code_add_line(func, index, addition, **kwargs):
@@ -101,32 +171,20 @@ def code_add_lines(func, *additions, **kwargs):
     :param after:     add line after the given index, not before
     :return:          whether code was modified or not
     """
-    a = additions
     after = kwargs.pop("after", False)
-    if len(a) % 2 != 0 or not \
-        all(isinstance(a[i], int) and isinstance(a[i+1], string_types) \
-            for i in range(0, len(a), 2)):
-        raise PatchError("Bad code additions")
-    indices = [a[i] for i in range(0, len(a), 2)]
-    if len(indices) != len(set(indices)):
-        raise PatchError("Bad code replacements")
     old_code = code_source(func)
     new_code = old_code.splitlines()
-    a = [(a[i], a[i+1]) for i in range(0, len(a), 2)]
-    for i in sorted(a, key=lambda x: x[0], reverse=True):
-        n, addition = i
+    for n, a in __sort_int_text_pairs(old_code, additions, "addition"):
         if after:
             n += 1
         if n == 0:
-            new_code.insert(0, addition.lstrip())
+            new_code.insert(0, a.lstrip())
         else:
             # automatically compute indentation if not provided in added line
-            if len(addition) == len(addition.lstrip()):
-                addition = __get_block_indent(new_code, n) + addition
-            new_code.insert(n, addition)
-    new_code = "\n".join(new_code)
-    patchy.api._set_source(func, new_code)
-    return old_code != new_code
+            if len(a) == len(a.lstrip()):
+                a = __get_block_indent(new_code, n) + a
+            new_code.insert(n, a)
+    return __apply_code(func, old_code, new_code)
 code_insert_lines = code_add_lines
 
 
@@ -177,6 +235,7 @@ def code_replace(func, *replacements):
         for i in range(0, len(replacements), 2):
             new_code = new_code.replace(replacements[i], replacements[i+1])
         patchy.replace(func, old_code, new_code)
+    # report whether the code was changed or not
     return old_code != new_code
 
 
@@ -198,33 +257,16 @@ def code_replace_lines(func, *replacements):
     :return:             whether code was modified or not
     """
     r = replacements
-    if len(r) % 2 != 0 or not \
-        all(isinstance(r[i], int) and (isinstance(r[i+1], string_types) or \
-            r[i+1] is None) for i in range(0, len(r), 2)):
-        raise PatchError("Bad code replacements")
-    indices = [r[i] for i in range(0, len(r), 2)]
-    if len(indices) != len(set(indices)):
-        raise PatchError("Bad code replacements")
     old_code = code_source(func)
     new_code = old_code.splitlines()
-    delete = []
-    for i in range(0, len(r), 2):
-        n, replacement = r[i:i+2]
-        if replacement is None:
-            delete.append(n)
+    for n, r in __sort_int_text_pairs(old_code, replacements, "replacement"):
+        if r is None:
+            new_code.pop(n)
         else:
             l = new_code[n]
             indent = l[:len(l)-len(l.lstrip())]
-            new_code[n] = indent + replacement.lstrip()
-    nref = 0
-    for i in sorted(delete, reverse=True):
-        if i < 0:
-            i = min(-1, nref)
-            nref = -i
-        del new_code[i]
-    new_code = "\n".join(new_code)
-    patchy.api._set_source(func, new_code)
-    return old_code != new_code
+            new_code[n] = indent + r.lstrip()
+    return __apply_code(func, old_code, new_code)
 
 
 def code_restore(func):
