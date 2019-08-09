fac2376fb4c71af504c771bb252e419cc27d863d
dhondta <alexandre.dhondt@gmail.com>
2019-08-09 14:38:46 +0200

    New release

--- tinyscript/helpers/__init__.py
+++ tinyscript/helpers/__init__.py
@@ -7,6 +7,7 @@ from .datatrans import *
 from .licenses import *
 from .patch import *
 from .termsize import *
+from .timeout import *
 from .types import *
 from .utils import *
 
@@ -14,9 +15,10 @@ from .datatrans import __features__ as _datatrans
 from .licenses import __features__ as _licenses
 from .patch import __features__ as _patch
 from .termsize import __features__ as _termsize
+from .timeout import __features__ as _timeout
 from .types import __features__ as _types
 from .utils import __features__ as _utils
 
 
 __all__ = __features__ = _datatrans + _licenses + _patch + _termsize + \
-                         _types + _utils
+                         _timeout + _types + _utils
