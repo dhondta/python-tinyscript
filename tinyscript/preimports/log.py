b887d7f992eba2cefdec40e5a8a0a5433bcad5ba
dhondta <alexandre.dhondt@gmail.com>
2019-11-07 09:26:15 +0100

    Improved preimports.log

--- tinyscript/preimports/log.py
+++ tinyscript/preimports/log.py
@@ -7,22 +7,30 @@ import logging
 
 
 def __set_logger(glob, *names):
+    """
+    Set up the loggers with the given names according to Tinyscript's logging
+     configuration.
+    
+    :param glob:  globals dictionary
+    :param names: logger names
+    """
     if len(names) == 0:
         names = [None]
+    main = glob['logger']
     for name in names:
         logger = logging.getLogger(name)
-        dl = glob['args']._debug_level
-        for h in logger.handlers:
-            h.setFormatter(logging.Formatter(glob['LOG_FORMAT'],
-                                             glob['DATE_FORMAT']))
-        logger.setLevel(dl)
-        coloredlogs.install(dl,
-                            logger=logger,
-                            fmt=glob['LOG_FORMAT'],
-                            datefmt=glob['DATE_FORMAT'], 
-                            milliseconds=glob['TIME_MILLISECONDS'],
-                            syslog=glob['args']._debug_syslog,
-                            stream=glob['args']._debug_logfile)
+        # check first that the given logger is not Tinyscript's one
+        if main.name == name or id(main) == id(logger):
+            continue
+        # set Tinyscript's logger as the logger's parent
+        logger.parent = main
+        # copy the reference to the list of handlers
+        logger.handlers = main.handlers
+        # disable propagation from the sublogger so that it does not duplicate
+        #  log messages
+        logger.propagate = False
+        # ensure that the main logger has no parent
+        main.parent = None
 
 
 logging.setLogger = __set_logger
