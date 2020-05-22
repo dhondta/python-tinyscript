051266664af6853cbe18345e65fb661149fa11c4
dhondta <alexandre.dhondt@gmail.com>
2020-05-22 16:22:44 +0200

    Improved helpers.data.types.strings

--- tinyscript/helpers/data/types/strings.py
+++ tinyscript/helpers/data/types/strings.py
@@ -5,8 +5,7 @@
 import ast
 import re
 from six import binary_type, string_types
-from string import ascii_lowercase as LC, ascii_uppercase as UC, \
-                   ascii_letters as letters, digits, printable, punctuation
+from string import ascii_lowercase as LC, ascii_uppercase as UC, ascii_letters as letter, digits, printable, punctuation
 
 
 __all__ = __features__ = []
@@ -45,12 +44,12 @@ def _is_from_alph(s, a, t):
 
 
 # various string-related check functions
-__all__ += ["is_str", "is_bytes", "is_digits", "is_letters", "is_lowercase",
-            "is_printable", "is_punctuation", "is_uppercase"]
+__all__ += ["is_str", "is_bytes", "is_digits", "is_letters", "is_lowercase", "is_printable", "is_punctuation",
+            "is_uppercase"]
 is_str         = lambda s: isinstance(s, string_types)
 is_bytes       = lambda s: isinstance(s, binary_type)
 is_digits      = lambda s, t=1.0: _is_from_alph(s, digits, t)
-is_letters     = lambda s, t=1.0: _is_from_alph(s, letters, t)
+is_letters     = lambda s, t=1.0: _is_from_alph(s, letter, t)
 is_lowercase   = lambda s, t=1.0: _is_from_alph(s, LC, t)
 is_printable   = lambda s, t=1.0: _is_from_alph(s, printable, t)
 is_punctuation = lambda s, t=1.0: _is_from_alph(s, punctuation, t)
@@ -58,15 +57,13 @@ is_uppercase   = lambda s, t=1.0: _is_from_alph(s, UC, t)
 
 # various data format check functions
 __all__ += ["is_bin", "is_hex"]
-is_bin = lambda b: is_str(b) and all(set(_).difference(set("01")) == set() \
-                                     for _ in re.split(r"\W+", b))
-is_hex = lambda h: is_str(h) and len(h) % 2 == 0 and \
-                   set(h.lower()).difference(set("0123456789abcdef")) == set()
+is_bin = lambda b: is_str(b) and all(set(_).difference(set("01")) == set() for _ in re.split(r"\W+", b)) or \
+                   isinstance(b, (list, set, tuple)) and all(str(x) in "01" for x in b)
+is_hex = lambda h: is_str(h) and len(h) % 2 == 0 and set(h.lower()).difference(set("0123456789abcdef")) == set()
 
 # some other common check functions
 __all__ += ["is_long_opt", "is_short_opt"]
-is_long_opt  = lambda o: is_str(o) and \
-                         re.match(r"^--[a-z]+(-[a-z]+)*$", o, re.I)
+is_long_opt  = lambda o: is_str(o) and re.match(r"^--[a-z]+(-[a-z]+)*$", o, re.I)
 is_short_opt = lambda o: is_str(o) and re.match(r"^-[a-z]$", o, re.I)
 
 
@@ -75,15 +72,15 @@ __all__ += ["str_contains", "str_matches"]
 
 
 def str_contains(alphabet, threshold=1.0):
-    """ Counts the characters of a string and determines, given an alphabet, if
-         the string has enough valid characters. """
+    """ Counts the characters of a string and determines, given an alphabet, if the string has enough valid characters.
+    """
     if threshold < 0.0 or threshold > 1.0:
         raise ValueError("Bad threshold (should be between 0 and 1)")
     def _validation(s):
         p = sum(int(c in alphabet) for c in s) / float(len(s))
         if p < threshold:
-            raise ValueError("Input string does not contain enough items from "
-                             "the given alphabet ({:.2f}%)".format(p * 100))
+            raise ValueError("Input string does not contain enough items from the given alphabet ({:.2f}%)"
+                             .format(p * 100))
         return s
     return _validation
 
@@ -95,3 +92,4 @@ def str_matches(pattern, flags=0):
             raise ValueError("Input string does not match the given regex")
         return s
     return _validation
+
