5b015daf6e868a89be3fa9e1dc65b80a7c16a66c
dhondta <alexandre.dhondt@gmail.com>
2020-07-31 09:54:11 +0200

    Improved tinyscript.helpers.constants

--- tinyscript/helpers/constants.py
+++ tinyscript/helpers/constants.py
@@ -20,14 +20,15 @@ ADMIN = USER == ["root", "Administrator"][WINDOWS]
 __all__ += ["ENCODING", "LANGUAGE"]
 LANGUAGE, ENCODING = getlocale()
 
-__all__ += ["IPYTHON", "JUPYTER", "JYTHON", "PYPY", "PYTHON2", "PYTHON3"]
+__all__ += ["IPYTHON", "JUPYTER", "JYTHON", "PYPY", "PYTHON2", "PYTHON3", "TTY"]
 JYTHON  = sys.platform.startswith("java")
 PYPY    = hasattr(sys, "pypy_version_info")
 PYTHON2 = sys.version_info < (3,)
 PYTHON3 = sys.version_info > (3,)
-IPYTHON = JUPYTER = True
+TTY     = sys.stdout.isatty()
 try:
     __IPYTHON__
+    IPYTHON = JUPYTER = True
 except NameError:
     IPYTHON = JUPYTER = False
 
