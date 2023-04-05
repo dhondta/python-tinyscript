8fc6fb2f4e33e9c1ca68bcd0a697a6dd93d0bfa2
dhondta <alexandre.dhondt@gmail.com>
2023-04-06 00:13:34 +0200

    Refined __info__

--- src/tinyscript/__info__.py
+++ src/tinyscript/__info__.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 """Tinyscript package information.
 
 """
 import os
-from datetime import date
+from datetime import datetime
+
+__y = str(datetime.now().year)
+__s = "2017"
 
 __author__    = "Alexandre D'Hondt"
+__copyright__ = "© {} A. D'Hondt".format([__y, __s + "-" + __y][__y != __s])
 __email__     = "alexandre.dhondt@gmail.com"
-__copyright__ = "© 2017-%d A. D'Hondt" % date.today().year
-__license__   = "gpl-3.0"
+__license__   = "GPLv3+ (https://www.gnu.org/licenses/gpl-3.0.fr.html)"
+__source__    = "https://github.com/dhondta/python-pybots"
 
 with open(os.path.join(os.path.dirname(__file__), "VERSION.txt")) as f:
     __version__ = f.read().strip()
