2c71af4c9694dc3e000bb11ac6e431d4bc21a6e5
dhondta <alexandre.dhondt@gmail.com>
2025-02-23 23:25:18 +0100

    Fixed issue with argparse

--- src/tinyscript/argreparse.py
+++ src/tinyscript/argreparse.py
@@ -737,6 +737,37 @@ class ArgumentParser(BaseArgumentParser, _NewActionsContainer):
         self._namespace._command = join(self._tokens)
         return self._namespace
     
+    def parse_known_args(self, args=None, namespace=None):
+        args = _sys.argv[1:] if args is None else list(args)
+        # default Namespace built from parser defaults
+        if namespace is None:
+            namespace = Namespace(self)
+        # add any action defaults that aren't present
+        for action in self._actions:
+            if action.dest is not SUPPRESS:
+                if not hasattr(namespace, action.dest):
+                    if action.default is not SUPPRESS:
+                        setattr(namespace, action.dest, action.default)
+        # add any parser defaults that aren't present
+        for dest in self._defaults:
+            if not hasattr(namespace, dest):
+                setattr(namespace, dest, self._defaults[dest])
+        # parse the arguments and exit if there are any errors
+        if self.exit_on_error:
+            try:
+                namespace, args = self._parse_known_args(args, namespace)
+            except ArgumentError as err:
+                self.error(str(err))
+        else:
+            namespace, args = self._parse_known_args(args, namespace)
+        if hasattr(namespace, _UNRECOGNIZED_ARGS_ATTR):
+            args.extend(set(getattr(namespace, _UNRECOGNIZED_ARGS_ATTR)))
+            try:
+                delattr(namespace, _UNRECOGNIZED_ARGS_ATTR)
+            except:
+                pass
+        return namespace, args
+    
     def print_extended_help(self, level=1, file=None):
         if not isinstance(self.details, (tuple, list, set)):
             self.details = [self.details]
